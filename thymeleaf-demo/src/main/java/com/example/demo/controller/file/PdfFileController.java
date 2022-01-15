d1fc59234f5383f9c16a2a50460c7b23bfea976c
刘政华 <liuzhenghua@liuzhenghuadeMacBook-Air.local>
2022-01-16 07:36:49 +0800

    add thymelead demo

---
+++ thymeleaf-demo/src/main/java/com/example/demo/controller/file/PdfFileController.java
@@ -0,0 +1,36 @@
+package com.example.demo.controller.file;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/pdf")
+public class PdfFileController {
+
+	
+	@GetMapping("/download/{filename}")
+	@ResponseBody
+	public void download(@PathVariable("filename")String filename, HttpServletResponse response) throws IOException {
+		String fullPath = "static/" + filename;
+		response.addHeader("Content-Type", "application/pdf");
+		InputStream in = Thread.currentThread().getContextClassLoader().getResourceAsStream(fullPath);
+		OutputStream out = response.getOutputStream();
+		byte[] buf = new byte[512];
+		while (in.read(buf) != -1) {
+			out.write(buf);
+		}
+		in.close();
+		out.flush();
+		out.close();
+		System.out.println(filename);
+	}
+}
